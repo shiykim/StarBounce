d8bd5c2fbd2f8f0a652a7649e850e5071f6e9127
claire <shiykim@gmail.com>
2018-09-26 22:44:18 -0400

    Add seesaw

--- canvas.js
+++ canvas.js
@@ -3,15 +3,17 @@ let ctx = canvas.getContext("2d");
 
 let rightPressed = false;
 let leftPressed = false;
-let space = false;
-let rightPressedRight = false;
-let leftPressedRight = false;
-let spaceRight = false;
+let down = false;
+let up = false;
+// let rightPressedRight = false;
+// let leftPressedRight = false;
+// let downRight = false;
 
 let player1X = 100;
 let player1Y = 70;
 // let player1Width = 120;
-let player2X = 450;
+let player2X = 480;
+let player2Y = 480;
 // let player2Width = 120;
 
 let score = 0;
@@ -33,26 +35,46 @@ rightB.src = 'images/branch-right.png';
 player1.src = 'images/junimo1.png';
 player2.src = 'images/junimo2.png';
 
+// let myAudio = new Audio();
+// myAudio.src = 'sounds/comes_alive.mp3';
+// myAudio.play();
+// myAudio.addEventListener('ended', function() {
+//     this.currentTime = 0;
+//     this.play();
+// }, false);
 
 function foreground() {
   ctx.drawImage(background, 0, 0, 1300, 2000, 0, 0, 739, 1830);
   ctx.drawImage(leftB, 0, 0, 1936, 3000, 0, 70, 520, 700);
   ctx.drawImage(rightB, 0, 0, 1600, 3000, 350, 70, 520, 700);
+  ctx.beginPath();
   ctx.font = "20px Arial";
   ctx.fillText("Score Board", 280, 100);
+
+  ctx.save();
+  ctx.beginPath();
+  // ctx.rotate(10*Math.PI/180);
+  ctx.rotate(-8*Math.PI/180);
+  ctx.rect(-20,600,540,30);
+  // ctx.rect(160,490,540,30);
+  ctx.fillStyle = "brown";
+  ctx.fill();
+  ctx.restore();
 }
 
 function keyDownHandler(e) {
     if(e.keyCode === 68) {
       rightPressed = true;
     } else if (e.keyCode === 83) {
-      space = true;
+      down = true;
+    } else if (e.keyCode === 87) {
+      up = true;
     } else if (e.keyCode === 65) {
       leftPressed = true;
-    }// } else if (e.keyCode === 76) {
+    } // } else if (e.keyCode === 76) {
     //   rightPressedRight = true;
     // } else if (e.keyCode === 75) {
-    //   spaceRight = true;
+    //   downRight = true;
     // } else if (e.keyCode === 74) {
     //   leftPressedRight = true;
     // }
@@ -62,13 +84,15 @@ function keyUpHandler(e) {
     if(e.keyCode === 68) {
         rightPressed = false;
     } else if(e.keyCode === 83) {
-        space = false;
+        down = false;
+    } else if (e.keyCode === 87) {
+        up = false;
     } else if(e.keyCode === 65) {
         leftPressed = false;
     } // } else if (e.keyCode === 76) {
     //   rightPressedRight = false;
     // } else if (e.keyCode === 75) {
-    //   spaceRight = false;
+    //   downRight = false;
     // } else if (e.keyCode === 74) {
     //   leftPressedRight = false;
     // }
@@ -119,23 +143,45 @@ function fruitsGoLeft(){
 
 function players(){
   ctx.drawImage(player2, 0, 0, 1600, 3000, player1X, player1Y, 520, 700);
-  // ctx.drawImage(player2, 0, 0, 1600, 3000, player2X, 70, 520, 700);
+  ctx.drawImage(player2, 0, 0, 1600, 3000, player2X, player2Y, 520, 700);
 
-  if (rightPressed && (player1X < 180) && (player2X < 560)) {
+  if (rightPressed && (player1X < 180) && (player2X < 540)) {
       player1X += 7;
       player2X += 7;
   } else if (leftPressed && (player1X > 0) && (player2X > 400)) {
      player1X -= 7;
      player2X -= 7;
-  } else if (space){
-    while (player1Y < 500){
-      player1Y += 10;
+  } else if (down && player1Y < 480){
+      while (player1Y < 480){
+        player1Y += 10;
+        player2Y -= 10;
+        // setInterval((ctx.drawImage(player2, 0, 0, 1600, 3000, player1X, player1Y, 520, 700)));
+      }
+  } else if (up && player1Y >= 70){
+    while (player1Y >= 70){
+      player1Y -= 10;
+      player2Y += 10;
+      // setInterval((ctx.drawImage(player2, 0, 0, 1600, 3000, player1X, player1Y, 520, 700)));
     }
   }
 
-function fallingPlayer(){
-  player1Y += 10;
-}
+
+    // } else if (down && player1Y == 500){
+    //   while (player1Y > 70){
+    //     player1Y -= 10;
+    //     player2Y += 10;
+    //     // setInterval((ctx.drawImage(player2, 0, 0, 1600, 3000, player1X, player1Y, 520, 700)));
+    //   }
+  }
+//
+// function fallingPlayer(){
+//   this.update = function(){
+//     dx += 5;
+//     this.draw();
+//   };
+//   while player1Y <
+//   this.update();
+// }
 
 
   // two player logic -> clogs up the handle events
@@ -145,8 +191,6 @@ function fallingPlayer(){
   //   } else if (leftPressedRight && player2X > 450 ) {
   //     player2X -= 7;
   // }
-
-}
 
 
 // let fruitsArrayRight = [];
@@ -174,7 +218,6 @@ function animate(){
   for (let i = 0; i < fruitsArray.length; i++) {
     fruitsArray[i].update();
   }
-
   // for (let i = 0; i < fruitsArrayRight.length; i++) {
   //   fruitsArrayRight[i].update();
   // }
@@ -182,5 +225,5 @@ function animate(){
 }
 
 animate();
-setInterval(fruitsGoLeft, 1200);
+// setInterval(fruitsGoLeft, 1200);
 // setInterval(fruitsGoRight, 1200);
